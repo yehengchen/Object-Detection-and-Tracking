977f550baae56ac6845d978f50269a6ed96c7491
Bobby Chen <627955504@qq.com>
2019-07-16 11:20:12 +0900

    Update yolo.py

--- OneStage/yolo/deep_sort_yolov3/yolo.py
+++ OneStage/yolo/deep_sort_yolov3/yolo.py
@@ -9,7 +9,7 @@ import os
 import random
 from timeit import time
 from timeit import default_timer as timer  ### to calculate FPS
-
+import cv2
 import numpy as np
 from keras import backend as K
 from keras.models import load_model
@@ -17,18 +17,39 @@ from PIL import Image, ImageFont, ImageDraw
 
 from yolo3.model import yolo_eval
 from yolo3.utils import letterbox_image
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input",help="path to input video", default = "./test_video/det_t1_video_00315_test.avi")
+ap.add_argument("-c", "--class",help="name of class", default = "person")
+args = vars(ap.parse_args())
 
 class YOLO(object):
     def __init__(self):
-        self.model_path = './model_data/yolo.h5'
+        self.model_path = './model_data/yolo_cc_0612.h5'
         self.anchors_path = 'model_data/yolo_anchors.txt'
         self.classes_path = 'model_data/obj.txt'
-        self.score = 0.6
-        self.iou = 0.5
+        if args["class"] == 'person':
+           self.score = 0.6 #0.8
+           self.iou = 0.6
+           self.model_image_size = (1120,1120)#(1120, 1120)
+        if args["class"] == 'car':
+           self.score = 0.6
+           self.iou = 0.6
+           self.model_image_size = (416, 416)
+        if args["class"] == 'bicycle' or args["class"] == 'motorcycle':
+           self.score = 0.6
+           self.iou = 0.6
+           self.model_image_size = (416, 416)
+
+        if args["class"] == 'fire_extinguisher' or args["class"] == 'fireplug':
+           self.score = 0.4#0.4
+           self.iou = 0.6
+           self.model_image_size = (416, 416)
+
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
         self.sess = K.get_session()
-        self.model_image_size = (416, 416) # fixed size or (None, None)
+        #self.model_image_size = (416, 416) # fixed size or (None, None) small targets:(320,320) mid targets:(960,960)
         self.is_fixed_size = self.model_image_size != (None, None)
         self.boxes, self.scores, self.classes = self.generate()
 
@@ -37,6 +58,7 @@ class YOLO(object):
         with open(classes_path) as f:
             class_names = f.readlines()
         class_names = [c.strip() for c in class_names]
+        #print(class_names)
         return class_names
 
     def _get_anchors(self):
@@ -95,14 +117,26 @@ class YOLO(object):
                 K.learning_phase(): 0
             })
         return_boxs = []
+        return_class_name = []
+        person_counter = 0
         for i, c in reversed(list(enumerate(out_classes))):
             predicted_class = self.class_names[c]
-            if predicted_class != 'person':
-                continue
-            #if predicted_class != 'person'and predicted_class != 'car':
+            #print(self.class_names[c])
+            '''
+            if predicted_class != 'person' and predicted_class != 'car':
+               print(predicted_class)
+               continue
+            '''
+            if predicted_class != args["class"]:#and predicted_class != 'car':
+               #print(predicted_class)
+               continue
+
+            person_counter += 1
+            #if  predicted_class != 'car':
                 #continue
+            #label = predicted_class
             box = out_boxes[i]
-           # score = out_scores[i]
+            #score = out_scores[i]
             x = int(box[1])
             y = int(box[0])
             w = int(box[3]-box[1])
@@ -114,8 +148,11 @@ class YOLO(object):
                 h = h + y
                 y = 0
             return_boxs.append([x,y,w,h])
-
-        return return_boxs
+            #print(return_boxs)
+            return_class_name.append([predicted_class])
+        #cv2.putText(image, str(self.class_names[c]),(int(box[0]), int(box[1] -50)),0, 5e-3 * 150, (0,255,0),2)
+        #print("Found person: ",person_counter)
+        return return_boxs,return_class_name
 
     def close_session(self):
         self.sess.close()
