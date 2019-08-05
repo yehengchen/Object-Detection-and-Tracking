3a738fbb647b17e5ede117b2af5b28f1fda92fed
Bobby Chen <627955504@qq.com>
2019-08-05 16:52:59 +0900

    Update voc_label.py

--- OneStage/yolo/yolov3/voc_label.py
+++ OneStage/yolo/yolov3/voc_label.py
@@ -4,7 +4,7 @@ import os
 from os import listdir, getcwd
 from os.path import join
 
-#我的项目中有6个类别，类别名称在这里修改
+#我的项目中有6个类别，类别名称在这里修改 change your classes in here
 classes = ["people","fire_extinguisher","fireplug","car","bicycle","motorcycle"]
 def convert(size, box):
     dw = 1./size[0]
@@ -20,9 +20,9 @@ def convert(size, box):
     return (x,y,w,h)
 
 def convert_annotation(image_id):
-    #这里改为.xml文件夹的路径
+    #这里改为.xml文件夹的路径 change the pth
     in_file = open('/home/cai/Desktop/dataset/data/Annotations/%s.xml'%(image_id))
-    #这里是生成每张图片对应的.txt文件的路径
+    #这里是生成每张图片对应的.txt文件的路径 change the pth
     out_file = open('/home/cai/Desktop/yolo_dataset/objectdetection/labels/%s.txt'%(image_id),'w')
     tree=ET.parse(in_file)
     root = tree.getroot()
@@ -39,20 +39,20 @@ def convert_annotation(image_id):
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
         bb = convert((w,h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
-#这里是train.txt文件的路径
+#这里是train.txt文件的路径 change the pth
 image_ids_train = open('/home/cai/Desktop/yolo_dataset/objectdetection/train.txt').read().strip().split()
-#这里是val.txt文件的路径
+#这里是val.txt文件的路径 change the pth
 image_ids_val = open('/home/cai/Desktop/yolo_dataset/objectdetection/val.txt').read().strip().split()
 
 list_file_train = open('object_train.txt', 'w')
 list_file_val = open('object_val.txt', 'w')
 for image_id in image_ids_train:
-    #这里改为样本图片所在文件夹的路径
+    #这里改为样本图片所在文件夹的路径 change the pth
     list_file_train.write('/home/cai/Desktop/yolo_dataset/objectdetection/JPEGImages/%s.jpg\n'%(image_id))
     #convert_annotation(image_id)
 list_file_train.close()
 for image_id in image_ids_val:
-    #这里改为样本图片所在文件夹的路径
+    #这里改为样本图片所在文件夹的路径 change the pth
     list_file_val.write('/home/cai/Desktop/yolo_dataset/objectdetection/JPEGImages/%s.jpg\n'%(image_id))
     #convert_annotation(image_id)
 list_file_val.close()
